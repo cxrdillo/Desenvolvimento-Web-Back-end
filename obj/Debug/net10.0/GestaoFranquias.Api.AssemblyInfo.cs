34ddfee4300c351e92acb9aa8cb46d9a9608abba
Cardillo <gabriel.cardillo@hotmail.com>
2026-09-12 18:07:20 -0300

    fix: correção do README

--- obj/Debug/net10.0/GestaoFranquias.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/GestaoFranquias.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoFranquias.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e4dc1da76fbdde3052f4345d90db9906698c5ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoFranquias.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoFranquias.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
